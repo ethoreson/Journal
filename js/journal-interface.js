81d0a58b23b3da51be654feb2df006ba63c24d14
Emilie Thoreson <thoresonemilie@gmail.com>
2017-07-31 11:57:12 -0700

    backend logic for first sentence only

--- js/journal-interface.js
+++ js/journal-interface.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
     var title = $('#title').val();
     var body = $('#body').val();
     var newEntry = new Journal();
+    var firstSentence = newEntry.getTeaser(body);
     var numberOfWords = newEntry.numberOfWords(body);
     var numberOfVowels = newEntry.getVowels(body);
     var numberOfConsonants = newEntry.getConsonants(body);
+      $('#repost').append("<h2><li>" + title + "</li></h2>");
+      $('#repost').append("<h3><li>" + firstSentence + "</li></h3>");
+      $('#repost').append("<h4><li>" + body + "</li></h4>");
       $('#post').append("<li>Number of Words: " + numberOfWords + "</li>");
       $('#post').append("<li>Number of Vowels: " + numberOfVowels + "</li>");
       $('#post').append("<li>Number of Consonants: " + numberOfConsonants + "</li>");
--- js/journal.js
+++ js/journal.js
@@ -3,19 +3,24 @@ function Journal() {
 
 Journal.prototype.numberOfWords = function(entry) {
   return entry.split(' ').length;
-}
+};
 
 Journal.prototype.getVowels = function(entry) {
   var count = entry.match(/[aeiou]/gi);
   return count === null ? 0 : count.length;
-}
+};
 
 Journal.prototype.getConsonants = function(entry) {
-  var entry = entry.replace(/\s+/g, '');
-  var entry = entry.replace(/[^a-zA-Z]/gi, '')
-  var countString = entry.split('').length;
+  var entryone = entry.replace(/\s+/g, '');
+  var entrytwo = entryone.replace(/[^a-zA-Z]/gi, '');
+  var countString = entrytwo.split('').length;
   var count = (countString - this.getVowels(entry));
   return count;
+};
+
+Journal.prototype.getTeaser = function(entry) {
+  var firstSentence = entry.match((.*?)\.);
+  return firstSentence;
 }
 
 exports.journalModule = Journal;
